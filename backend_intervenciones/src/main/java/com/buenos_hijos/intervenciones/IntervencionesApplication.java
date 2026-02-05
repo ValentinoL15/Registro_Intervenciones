54ebf6a3c29efe1bcf22526a3d6e53f0ae1c58d6
ValentinoL15 <pichilongo1@gmail.com>
2026-02-05 14:06:38 -0300

    primer commit

---
+++ backend_intervenciones/src/main/java/com/buenos_hijos/intervenciones/IntervencionesApplication.java
@@ -0,0 +1,13 @@
+package com.buenos_hijos.intervenciones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntervencionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntervencionesApplication.class, args);
+	}
+
+}
