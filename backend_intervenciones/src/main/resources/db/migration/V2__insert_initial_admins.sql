f10f72b3e3fb2f9019f573299646aa8f8fa8f2d2
ValentinoL15 <pichilongo1@gmail.com>
2026-03-09 00:22:16 -0300

    primera version

--- backend_intervenciones/src/main/resources/db/migration/V2__insert_initial_admins.sql
+++ backend_intervenciones/src/main/resources/db/migration/V2__insert_initial_admins.sql
@@ -1,15 +1,15 @@
 -- Insertar Usuario 1
 INSERT INTO user (name, lastname, username, email, password, role, active)
-VALUES ('Valentino', 'Longo', 'Valen', 'Pichilongo1@gmail.com', '$2a$10$6A45K7Kd3qmrdgS1A2iXte3TgfXQK/2GBpX2zapxYD4EqTR.JmbwO', 'ADMIN', 1);
+VALUES ('Maria José', 'Unno', 'Mariajo', 'lic.mariajoseunno@gmail.com', '$2a$10$CJkZ5eqwUHcDiNL35PW4Z.YsDBnAsrxHBU99t/ew90MK3syVa7t1q', 'ADMIN', 1);
 -- Asociarlo a la tabla admins (LAST_INSERT_ID() toma el ID generado arriba)
 INSERT INTO admins (user_id) VALUES (LAST_INSERT_ID());
 
 -- Insertar Usuario 2
 INSERT INTO user (name, lastname, username, email, password, role, active)
-VALUES ('Claudia', 'Benestante', 'Claudia', 'admin2@escuela.com', '$2a$10$F8RZWO3aI/AD5yjAFI5TZe4Vfj..g4MKvRSzfmDXwOc82BP4Br5CC', 'ADMIN', 1);
+VALUES ('Claudia', 'Benestante', 'Claudia', 'claubenestante@hotmail.com', '$2a$10$RolMu5Tn/mhl.5eUGb75m.MsuINX2d.ZRU.wMYDI/.KTo0a12yKLC', 'ADMIN', 1);
 INSERT INTO admins (user_id) VALUES (LAST_INSERT_ID());
 
 -- Insertar Usuario 3
 INSERT INTO user (name, lastname, username, email, password, role, active)
-VALUES ('Valeria', 'Perez', 'Valeria', 'admin3@escuela.com', '$2a$10$YfAUCL5Xnkm.dnLpBQ8kkO7fFs4tpaDX7mOovtINWd1oWtMMWv20W', 'ADMIN', 1);
+VALUES ('Valeria', 'Bachanini', 'Valeria', 'valebachanini@yahoo.com.ar', '$2a$10$EE9zCl2EZ7op6MoeMbbwvOvfdaA91agH5TOTbjKKvig9wBAtq0XLG', 'ADMIN', 1);
 INSERT INTO admins (user_id) VALUES (LAST_INSERT_ID());
